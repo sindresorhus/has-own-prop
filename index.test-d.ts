990f55b05e4ef01fdd7078185c3727c67f658f6d
hiroki osame <hiroki.osame@gmail.com>
2021-11-01 05:23:40 -0400

    Include a type guard in the TypeScript types (#4)
    Co-authored-by: Sindre Sorhus <sindresorhus@gmail.com>

--- index.test-d.ts
+++ index.test-d.ts
@@ -4,3 +4,8 @@ import hasOwnProperty from './index.js';
 expectType<boolean>(hasOwnProperty({}, 'hello'));
 expectType<boolean>(hasOwnProperty([1, 2, 3], 0));
 expectType<boolean>(hasOwnProperty('foo', Symbol('bar')));
+
+const someObject = {};
+if (hasOwnProperty(someObject, 'a') && hasOwnProperty(someObject, 'b')) {
+	expectType<Record<'a', unknown> & Record<'b', unknown>>(someObject);
+}
